f76f431421f0cc033a1b53950da68b5dcbb04fe2
Eric Johnson <roerjo.personal@gmail.com>
2021-08-17 11:56:34 -0400

    Add S3 policy

--- .infrastructure/aws_s3.tf
+++ .infrastructure/aws_s3.tf
@@ -1,4 +1,31 @@
 resource "aws_s3_bucket" "pipeline_bucket" {
   bucket = "${var.app_name}-codepipeline"
-  acl    = "private"
+}
+
+data "aws_iam_policy_document" "s3" {
+  statement {
+    sid    = "s3"
+    effect = "Allow"
+    principals {
+      identifiers = [
+        aws_iam_role.codebuild_role.arn,
+        aws_iam_role.codepipeline_role.arn,
+      ]
+      type = "AWS"
+    }
+    actions = [
+      "s3:Get*",
+      "s3:List*",
+      "s3:Put*"
+    ]
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.pipeline_bucket.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.pipeline_bucket.bucket}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "pipeline_bucket" {
+  bucket = aws_s3_bucket.pipeline_bucket.id
+  policy = data.aws_iam_policy_document.s3.json
 }
