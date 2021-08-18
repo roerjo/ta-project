bfad35f7ad89195fc3dfb2f0388993927534e132
Eric Johnson <roerjo.personal@gmail.com>
2021-08-18 15:08:40 -0400

    Codebuild working

--- .infrastructure/aws_codebuild.tf
+++ .infrastructure/aws_codebuild.tf
@@ -5,6 +5,7 @@ resource "aws_codebuild_project" "build" {
   source {
     type     = "S3"
     location = "${aws_s3_bucket.pipeline_bucket.bucket}/source.zip"
+    buildspec = file("buildspec/buildspec.yml")
   }
 
   environment {
@@ -12,15 +13,6 @@ resource "aws_codebuild_project" "build" {
     image           = "aws/codebuild/standard:5.0"
     type            = "LINUX_CONTAINER"
     privileged_mode = true
-    environment_variable {
-      name  = "IMAGE_REPO_NAME"
-      value = aws_ecr_repository.ecr.name
-    }
-    environment_variable {
-      name  = "IMAGE_TAG"
-      value = "latest"
-
-    }
   }
 
   artifacts {
@@ -29,7 +21,7 @@ resource "aws_codebuild_project" "build" {
 }
 
 resource "aws_iam_role" "codebuild_role" {
-  name = "friendemic-codebuild-role"
+  name = "${var.app_name}-codebuild-role"
 
   assume_role_policy = <<EOF
 {
@@ -46,4 +38,47 @@ resource "aws_iam_role" "codebuild_role" {
 }
 EOF
 
+}
+
+resource "aws_iam_policy" "codebuild_logging_policy" {
+  name = "codebuild-logging-policy"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": [
+        "*"
+      ],
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+    },
+    {
+      "Action": [
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:BatchGetImage",
+        "ecr:CompleteLayerUpload",
+        "ecr:GetAuthorizationToken",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:InitiateLayerUpload",
+        "ecr:PutImage",
+        "ecr:UploadLayerPart"
+      ],
+      "Resource": "*",
+      "Effect": "Allow"
+    }
+  ]
+}
+POLICY
+
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_role_policy_attach" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = aws_iam_policy.codebuild_logging_policy.arn
 }
